3552a3bd51a89f532178e2e57fa06d3bd7027a7e
Elya <elyasalmanovaasr@gmail.com>
2025-01-28 01:20:30 +0700

    NGG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,75 +1,64 @@
 package ru.netology.service;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
+    private CashbackHackService service;
+
+    @BeforeMethod
+    public void setup() {
+        service = new CashbackHackService();
+    }
+
     @Test
-    public void testShouldAmount1000() {
-        CashbackHackService service = new CashbackHackService();
+    public void shouldReturnZeroForAmount1000() {
         int amount = 1000;
-
+        int actual = service.remain(amount);
         int expected = 0;
-        int actual = service.remain(amount);
-
         assertEquals(actual, expected);
     }
 
     @Test
-    public void testShouldAmount999() {
-        CashbackHackService service = new CashbackHackService();
+    public void shouldReturnOneForAmount999() {
         int amount = 999;
-
+        int actual = service.remain(amount);
         int expected = 1;
-        int actual = service.remain(amount);
-
         assertEquals(actual, expected);
     }
 
     @Test
-    public void testShouldAmount1001() {
-        CashbackHackService service = new CashbackHackService();
+    public void shouldReturn999ForAmount1001() {
         int amount = 1001;
-
+        int actual = service.remain(amount);
         int expected = 999;
-        int actual = service.remain(amount);
-
         assertEquals(actual, expected);
     }
 
     @Test
-    public void testShouldAmountNegative() {
-        CashbackHackService service = new CashbackHackService();
+    public void shouldReturn500ForNegativeAmount() {
         int amount = -500;
-
-        int expected = 500;
         int actual = service.remain(amount);
-
+        int expected = 500;
         assertEquals(actual, expected);
     }
 
     @Test
-    public void testShouldAmountZero() {
-        CashbackHackService service = new CashbackHackService();
+    public void shouldReturn1000ForZeroAmount() {
         int amount = 0;
-
-        int expected = 1000;
         int actual = service.remain(amount);
-
+        int expected = 1000;
         assertEquals(actual, expected);
     }
 
-
     @Test
-    public void testShouldAmountLargeNumber() {
-        CashbackHackService service = new CashbackHackService();
+    public void shouldReturn500ForLargeNumber() {
         int amount = 1000500;
-
-        int expected = 500;
         int actual = service.remain(amount);
-
+        int expected = 500;
         assertEquals(actual, expected);
     }
 }
